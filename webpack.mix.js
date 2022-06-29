d9433a8359150017dc4a91b4fb8497004a278670
Ron Marsden <rm@maxwebsolutions.com>
2022-06-29 17:53:30 +0100

    v1.1.0 Modularized everything for maintainability Breakpoints can now be fully overridden Class prefixes are now customizable Replaced block comments with silent comments for a smaller non-minified build Updated README.md

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,13 @@
-let mix = require('laravel-mix');
+let mix      = require('laravel-mix');
+let minifier = require('minifier');
 
 // Set the public path
 mix.setPublicPath('dist');
 
 // Compile SCSS
 mix.sass('src/flexya.scss', 'css');
+
+// Minify
+mix.then(() => {
+    minifier.minify('dist/css/flexya.css');
+});
